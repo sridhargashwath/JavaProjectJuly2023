d990bb0fdd7ed1cd2c89c75516261da46155f10e
Sridhar GA <138233113+sridhargashwath@users.noreply.github.com>
2023-07-21 14:09:04 +0530

    day04
    Java Project

--- src/day04/Program01.java
+++ src/day04/Program01.java
@@ -3,8 +3,42 @@ package day04;
 public class Program01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		add(435,26);
+		add(425,26);
+		add(405,26.02345);
+		add(435,26);
+		add(435,27);
+		
 	}
+	//overloading size/datatype
+	public static void add(int a,int b)
+	{System.out.println(a+b);
+	}
+	public static void add(double a,double b)
+	{System.out.println(a+b);
+	}
+	public static void add(float a,float b)
+	{System.out.println(a+b);
+	}
+	
+	public static void add(int a,int b,int c)
+	{System.out.println(a+b+c);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	 
+	
+	
+		
+	}
+	
+	
 
 }
