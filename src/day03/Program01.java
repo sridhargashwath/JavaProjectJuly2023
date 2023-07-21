3613be529490830579239508a77f5ef228e16d0b
Sridhar GA <138233113+sridhargashwath@users.noreply.github.com>
2023-07-21 13:29:38 +0530

    Day04
    Java Project

--- src/day03/Program01.java
+++ src/day03/Program01.java
@@ -2,6 +2,7 @@ package day03;
 
 public class Program01 {
 
+	//Static Method
 	public static void main(String[] args) {
 		
 	String a="Good Morning";
@@ -10,14 +11,23 @@ public class Program01 {
 	display();
 	display();
 	display();
-	display();
+	
+	
+	  //x.disply();
+	
+	
+	
 	
 	}
-
+     // Static Method
 	protected static void display() {
 	System.out.println("Its a Good Year");
 	System.out.println("beautiful Day");
-		
+	
+
+	
 	}
+	//non-  Static Method 
+
 
 }
