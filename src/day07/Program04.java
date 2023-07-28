13b67efe1aa65b48579c3aac32b70d3a7c13465e
Sridhar GA <138233113+sridhargashwath@users.noreply.github.com>
2023-07-28 11:46:02 +0530

    day07
    Java Class

---
+++ src/day07/Program04.java
@@ -0,0 +1,20 @@
+package day07;
+
+public class Program04 {
+
+   public static void main(String[]args){
+
+	    System.out.println("______");
+        int a=101;
+        int b=20;
+        
+        if(a>b)
+        {
+        	System.out.println("Good Day.?");
+        }
+        System.out.println("_________");
+        
+        
+   }
+   
+}
---
+++ src/day07/Program05.java
@@ -0,0 +1,24 @@
+package day07;
+
+public class Program05 {
+
+   public static void main(String[]args){
+
+	    System.out.println("______");
+        int a=101;
+        int b=201;
+        
+        if(a==b)
+        {
+        	System.out.println("a is Greater");
+        }
+        else
+        {
+        System.out.println("b is Greater");
+        }
+        System.out.println("________");
+        
+        
+   }
+   
+}
